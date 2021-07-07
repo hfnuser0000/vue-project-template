4f8f701087427b678941ca4e6dc04277594873a3
Hoang Anh <hoanganh251098@gmail.com>
2021-07-07 09:23:59 +0700

    update: styles & local assets

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Vuex from 'vuex';
 import Element from 'element-ui'
 import AppMain from './App.vue';
 import Axios from 'axios';
+import './styles/element-ui.css';
+import './styles/layout.css';
 
 Vue.use(Element);
 
